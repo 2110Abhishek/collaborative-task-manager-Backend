baf8fb8c4024cac4e9ff74a38f9aeedf801cb551
2110Abhishek <agchoudhari2110@gmail.com>
2025-12-19 17:53:04 +0530

    Backend

--- src/app.ts
+++ src/app.ts
@@ -8,18 +8,30 @@ import userRoutes from "./routes/user.routes";
 
 const app = express();
 
-app.use(cors({
-  origin: process.env.CLIENT_URL,
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+
+      if (
+        origin.endsWith(".vercel.app") ||
+        origin === process.env.CLIENT_URL
+      ) {
+        return callback(null, true);
+      }
+
+      callback(new Error("CORS not allowed"));
+    },
+    credentials: true,
+  })
+);
+
 
 app.use(express.json());
 app.use(cookieParser());
-
 app.use("/api/auth", authRoutes);
 app.use("/api/tasks", taskRoutes);
 app.use("/api/users", userRoutes);
-
 app.use(errorMiddleware);
 
 export default app;
